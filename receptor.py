9e94391fa2f158f94e8f4292300d0adf9694153e
saortizag <saortizag@unal.edu.co>
2019-10-23 22:59:23 -0500

    receptor modified

--- receptor.py
+++ receptor.py
@@ -2,13 +2,18 @@ import Adafruit_BBIO.GPIO as GPIO
 import time
 import pickle
 
+recep_power = "P9_13"
 recep_input = "P9_11"
 GPIO.setup(recep_input, GPIO.IN)
+GPIO.setup(recep_power, GPIO.OUT)
 
 detections = []
 
+GPIO.output(recep_power, GPIO.HIGH)
 try:
+    
     while(True):
+    
         if GPIO.input(recep_input):
             detections.append(0)
         else:
@@ -16,6 +21,7 @@ try:
         time.sleep(0.02)
 
 except KeyboardInterrupt:
+    GPIO.output(recep_power, GPIO.LOW)
     with open("laser_data.pickle" , "wb") as handle:
         pickle.dump(detections, handle)
     GPIO.cleanup()
