e89502a3fc5195ceb53753a99df30ccaa36e73fa
saortizag <saortizag@unal.edu.co>
2019-11-05 19:22:21 -0500

    talanquera simulator added to receptor.py

--- receptor.py
+++ receptor.py
@@ -4,21 +4,31 @@ import pickle
 
 recep_power = "P9_13"
 recep_input = "P9_11"
-GPIO.setup(recep_input, GPIO.IN)
 GPIO.setup(recep_power, GPIO.OUT)
+GPIO.setup(recep_input, GPIO.IN)
 
-detections = []
+talanquera_power = "P9_22"
+talanquera_input = "P9_21"
+GPIO.setup(talanquera_power, GPIO.OUT)
+GPIO.setuo(talanquera_input, GPIO.IN)
+
+detections = {}
 
 GPIO.output(recep_power, GPIO.HIGH)
-try:
-    
-    while(True):
-    
-        if GPIO.input(recep_input):
-            detections.append(0)
+GPIO.output(talanquera_power, GPIO.HIGH)
+
+try:    
+    while(True):   
+        ii = 0
+        detections[ii] = []
+        if GPIO.input(talanquera_input):
+            if GPIO.input(recep_input): 
+                detections[ii].append(0)
+            else:
+                detections[ii].append(1)
+            time.sleep(0.02)
         else:
-            detections.append(1)
-        time.sleep(0.02)
+            ii+=1
 
 except KeyboardInterrupt:
     GPIO.output(recep_power, GPIO.LOW)
