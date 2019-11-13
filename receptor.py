db8141ae5b11b1e36b97bf00c83f61762f7c3e69
saortizag <saortizag@unal.edu.co>
2019-11-13 07:39:33 -0500

    date

--- receptor.py
+++ receptor.py
@@ -1,6 +1,8 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
+import datetime
 import pickle
+import numpy as np
 
 recep_power = "P9_13"
 recep_input = "P9_11"
@@ -20,12 +22,14 @@ GPIO.output(talanquera_power, GPIO.HIGH)
 try:    
     while(True):   
         ii = 0
+        now = []
         detections[ii] = []
         if GPIO.input(talanquera_input):
+            now.append(time.strftime("%d/%m/%Y %H:%M:%S"))
             if GPIO.input(recep_input): 
-                detections[ii].append(0)
+                detections[ii].append((np.nan,0))
             else:
-                detections[ii].append(1)
+                detections[ii].append(now,1)
             time.sleep(0.02)
         else:
             ii+=1
